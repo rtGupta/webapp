b9f2cd184724507c8415ae1b91e25d8fb76cd6dd
Aarti Gupta <gupta.aart@northeastern.edu>
2023-02-08 17:52:48 -0500

    User Schema improved for validations.

--- api/model/user.js
+++ api/model/user.js
@@ -1,54 +1,62 @@
 const User = (sequelize, DataTypes, Model) => {
-    const user = sequelize.define(
-      "User",
-      {
-        // Model attributes are defined here
-        id: {
-          type: DataTypes.INTEGER,
-          primaryKey: true,
-          autoIncrement: true,
-          allowNull: false,
-        },
-        firstName: {
-          type: DataTypes.STRING,
-          allowNull: false,
-          notEmpty: true
-        },
-        lastName: {
-          type: DataTypes.STRING,
-          allowNull: false,
-          notEmpty: true
-        },
-        username: {
-          type: DataTypes.STRING,
-          allowNull: false,
-          unique: true,
-          notEmpty: true
-        },
-        password: {
-          type: DataTypes.STRING,
-          allowNull: false,
-          notEmpty: true
-        },
-        createdAt: {
-          type: DataTypes.DATE,
-          allowNull: false,
-        },
-        updatedAt: {
-          type: DataTypes.DATE,
-          allowNull: false,
-        },
+  const user = sequelize.define(
+    "User",
+    {
+      // Model attributes are defined here
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+        readOnly: true
       },
-      {
-        // Other model options go here
-        sequelize, // We need to pass the connection instance
-        modelName: "User", // We need to choose the model name
-      }
-    );
-  
-    return user;
-  
-  };
-  
-  export default User;
-  
+      first_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        notEmpty: true,
+      },
+      last_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        notEmpty: true,
+      },
+      username: {
+        type: DataTypes.CITEXT,
+        allowNull: false,
+        unique: true,
+        notEmpty: true,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        notEmpty: true,
+        writeOnly: true
+      },
+      account_created: {
+        type: DataTypes.DATE,
+        readOnly: true,
+      },
+      account_updated: {
+        type: DataTypes.DATE,
+        readOnly: true,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        field: "account_created",
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        field: "account_updated",
+      },
+    },
+    {
+      // Other model options go here
+      sequelize, // We need to pass the connection instance
+      modelName: "User", // We need to choose the model name
+    }
+  );
+
+  return user;
+};
+
+export default User;
