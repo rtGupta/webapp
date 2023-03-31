d1a9eae4db9f797d3ce9450914bd81e0701fb55e
Aarti Gupta <gupta.aart@northeastern.edu>
2023-03-31 03:31:52 -0400

    Resolved application bootstrapping issue.

--- packer/setup_cloudwatch.sh
+++ packer/setup_cloudwatch.sh
@@ -1,8 +1,5 @@
 #! /bin/bash
 sudo yum update -y
 
-# Download the CloudWatch Agent
-sudo wget https://s3.amazonaws.com/amazoncloudwatch-agent/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm
-
-# Install the package
-sudo rpm -U ./amazon-cloudwatch-agent.rpm
+# Install Amazon Cloudwatch Agent
+sudo yum install amazon-cloudwatch-agent -y
